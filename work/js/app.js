5116e8af8bbb6ab15d8bdb160a5432b9562022b6
km1025 <matsuda.kanoko@gmail>
2024-09-29 14:13:00 +0900

    9/29

---
+++ work/js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const display = document.getElementById("display");
+  let expression = "";
+
+  // ボタンクリックで関数処理
+  document.getElementById('calculator').addEventListener('click', (e) => {
+    const target = e.target;
+
+  // 数字クリックで数字を表示
+  if(target.classList.contains('number')){
+    expression+=target.dataset.number;
+    display.value=expression;
+
+    //演算子の表示
+  }else if(target.classList.contains('operator')){
+    expression+=target.dataset.operator;
+    display.value=expression;
+
+  // クリアボタン
+  }else if(target.classList.contains('clear')){
+    expression='';
+    display.value=expression;
+  }else if(target.classList.contains('equals')){
+    
+    //数式を表示・イコール
+    try{
+      display.value=eval(expression);
+      expression=display.value
+    }catch{
+      display.value='error';
+      expressionz= '';
+    }
+  }
+  });
+});
